f06f177ea44286ada01cdb21d76b73cb06538503
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2025-12-07 05:16:58 +0900

    halfway d 1

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # advent-of-code-2025
 Attempting the challenge of https://adventofcode.com/2025
+ Blah blah blah!
 
-# copypasted here
-Advent of Code[About][Events][Shop][Settings][Log Out]animehunter123
-   $year=2025;[Calendar][AoC++][Sponsors][Leaderboards][Stats]
-Our sponsors help make Advent of Code possible:
-Jump Trading - We're traders, builders, engineers, and researchers obsessed with improving the models, tools, and systems that power everything we do. From raw silicon through order execution, we tackle problems few ever get to see.
-Starting this year, there will be 12 days of puzzles each December.
+
---
+++ day1/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+
+    // make a 100 number array?
+    let mut combo: Vec<u8> = Vec::new();
+    for i in 0..100 {
+        // println!("{i}");
+        combo.push(i);
+    }
+
+    // dbg!(combo);
+    // dbg!(combo.len());
+
+    let pointer = 0; // i will use this to keep track of where we pointed to in the combo vector
+    let password = 0; // the final password will be incremented if we pass over a zero
+
+    let file_path = Path::new("input.txt"); // Specify the path to your file
+
+    // Open the file and create a buffered reader
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    // Iterate over the lines in the file
+    for line_result in reader.lines() {
+        let line = line_result?; // Handle potential errors during line reading
+        // println!("{}", line); // Process or print each line
+        // if line.as_bytes()[0] == b'L'  {
+            // println!("FOUND A L!!")
+        // }
+
+    }
+
+    Ok(())
+}
+
